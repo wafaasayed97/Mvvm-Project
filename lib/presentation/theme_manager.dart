ae450c5f4dcfecc3d1b021ba73f43283ea6fffdb
wafaa <wafaasayed091@gmail.com>
2024-03-06 15:30:45 +0200

    - added text field form in theme manager - edited values manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -64,5 +64,30 @@ ThemeData getApplicationTheme() {
         ), //subtitle 1
         bodySmall: getRegularStyle(color: ColorManager.grey1), //caption
         bodyLarge: getRegularStyle(color: ColorManager.grey), //body text1
-      ));
+      ),
+      //Input Decoration Theme (Text Form Field)
+      inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.all(AppPadding.p8),
+          hintStyle: getRegularStyle(color: ColorManager.grey,fontSize: FontSize.s14),
+          labelStyle: getMediumStyle(color: ColorManager.grey ,fontSize: FontSize.s14),
+          errorStyle: getRegularStyle(color: ColorManager.error),
+          enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+          )));
 }
